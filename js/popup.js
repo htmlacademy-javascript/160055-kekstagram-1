76795d42ef0ef9a9359e08fd611bbebc3c1583ac
alex-bochkanov <abochkanov94@yandex.ru>
2024-02-26 19:19:10 +0300

    Add new functions

--- js/popup.js
+++ js/popup.js
@@ -3,6 +3,7 @@ const pictureModal = document.querySelector('.big-picture');
 const closeModalClass = pictureModal.querySelector('.big-picture__cancel');
 const hideCommentCount = document.querySelector('.social__comment-count');
 const hideCommentLoader = document.querySelector('.comments-loader');
+const bigPictureCommentsBlock = document.querySelector('.social__comments');
 
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
@@ -13,55 +14,62 @@ const onDocumentKeydown = (evt) => {
   }
 };
 
-const openPictureModal = (picture) => {
-  pictureModal.classList.remove('hidden');
-  hideCommentCount.classList.add('hidden');
-  hideCommentLoader.classList.add('hidden');
-  bodyClassPopup.classList.add('modal-open');
+closeModalClass.addEventListener('click', () => {
+  closePictureModal();
+});
+
+function closePictureModal() {
+  pictureModal.classList.add('hidden');
+  bodyClassPopup.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+  closeModalClass.removeEventListener('keydown', closePictureModal);
+}
+
+const renderComment = (comment) => {
+  const commentElement = document.createElement('li');
+  const commentElementImage = document.createElement('img');
+  const commentElementDescription = document.createElement('p');
+
+  commentElement.className = 'social__comment';
+  commentElementImage.className = 'social__picture';
+  commentElementImage.src = comment.avatar;
+  commentElementImage.alt = comment.name;
+  commentElementImage.width = 25;
+  commentElementImage.heigth = 25;
+  commentElementDescription.className = 'social__text"';
+  commentElementDescription.innerHTML = comment.message;
+  commentElement.appendChild(commentElementImage);
+  commentElement.appendChild(commentElementDescription);
+  bigPictureCommentsBlock.appendChild(commentElement);
+};
+
+const renderCommentsBlock = (comments) => {
+  bigPictureCommentsBlock.innerHTML = '';
+  comments.forEach((comment) => renderComment(comment));
+};
+
+const renderPopup = (picture) => {
   const bigPictureSrc = document.querySelector('.big-picture__img img');
   const bigPictureLikes = document.querySelector('.likes-count');
   const bigPictureCommentsCount = document.querySelector('.comments-count');
   const bigPictureDescription = document.querySelector('.social__caption');
-  const bigPictureCommentsBlock = document.querySelector('.social__comments');
 
   bigPictureSrc.src = picture.url;
   bigPictureLikes.textContent = picture.likes;
   bigPictureCommentsCount.textContent = picture.comments.length;
   bigPictureDescription.textContent = picture.description;
-  bigPictureCommentsBlock.innerHTML = '';
-  picture.comments.forEach((element) => {
-    const commentElement = document.createElement('li');
-    const commentElementImage = document.createElement('img');
-    const commentElementDescription = document.createElement('p');
+};
 
-    commentElement.className = 'social__comment';
-    commentElementImage.className = 'social__picture';
-    commentElementImage.src = element.avatar;
-    commentElementImage.alt = element.name;
-    commentElementImage.width = 25;
-    commentElementImage.heigth = 25;
-    commentElementDescription.className = 'social__text"';
-    commentElementDescription.innerHTML = element.message;
-    commentElement.appendChild(commentElementImage);
-    commentElement.appendChild(commentElementDescription);
-    bigPictureCommentsBlock.appendChild(commentElement);
-  });
+const openPictureModal = (picture) => {
+  pictureModal.classList.remove('hidden');
+  hideCommentCount.classList.add('hidden');
+  hideCommentLoader.classList.add('hidden');
+  bodyClassPopup.classList.add('modal-open');
+
+  renderPopup(picture);
+  renderCommentsBlock(picture.comments);
 
   document.addEventListener('keydown', onDocumentKeydown);
 };
 
-const closePictureModal = () => {
-  pictureModal.classList.add('hidden');
-  bodyClassPopup.classList.remove('modal-open');
-  document.removeEventListener('keydown', onDocumentKeydown);
-};
-closeModalClass.addEventListener('click', () => {
-  closePictureModal();
-});
-
-closeModalClass.addEventListener('keydown', (evt) => {
-  if (isEscapeKey(evt)) {
-    closePictureModal();
-  }
-});
-export {openPictureModal, closePictureModal};
+export {openPictureModal};

85a84552bc42250aa19431bea6f046e48a6a4bbd
alex-bochkanov <abochkanov94@yandex.ru>
2024-03-25 18:16:12 +0300

    Add filters buttons

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import {addPictures} from './picture-render.js';
+import {addPictures, addFiltersButtons} from './picture-render.js';
 import {registerUploadFileButton} from './form-upload-modal.js';
 import {picturesDownloadAlert} from './utils.js';
 import {getData} from './get-post-api.js';
@@ -6,6 +6,8 @@ import {getData} from './get-post-api.js';
 getData()
   .then((pictures) => {
     addPictures(pictures);
-  }).catch(picturesDownloadAlert);
+  })
+  .then(addFiltersButtons)
+  .catch(picturesDownloadAlert);
 
 registerUploadFileButton();
--- js/picture-render.js
+++ js/picture-render.js
@@ -1,6 +1,31 @@
 import {openPictureModal} from './popup.js';
+import {createRandomIdFromRangeGenerator} from './utils.js';
+
+// const NUMBER_OF_PICTURES = 10;
+const MAX_NUMBER_PICTURES = 25;
+
 const picturesRoot = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const imgSortFilters = document.querySelector('.img-filters');
+const buttonDiscussed = document.getElementById('filter-discussed');
+const buttonRandom = document.getElementById('filter-random');
+const buttonDefault = document.getElementById('filter-default');
+
+const setRandomIdPicture = (pictures) => {
+  const randomId = createRandomIdFromRangeGenerator(0, MAX_NUMBER_PICTURES);
+  pictures.id = randomId();
+};
+
+const getCommentsValue = (picture) => {
+  const commentsValue = picture.comments.length;
+  return commentsValue;
+};
+
+const compareComments = (commentsValueA, commentsValueB) => {
+  const rankA = getCommentsValue(commentsValueA);
+  const rankB = getCommentsValue(commentsValueB);
+  return rankB - rankA;
+};
 
 const createPicture = ({url, likes, comments}) => {
   const pictureElement = pictureTemplate.cloneNode(true);
@@ -9,6 +34,7 @@ const createPicture = ({url, likes, comments}) => {
   pictureElement.querySelector('.picture__comments').textContent = comments.length;
   return pictureElement;
 };
+
 const addPictures = (pictures) => {
   const fragment = document.createDocumentFragment();
   pictures.forEach((picture) => {
@@ -22,4 +48,27 @@ const addPictures = (pictures) => {
   picturesRoot.appendChild(fragment);
 };
 
-export {addPictures};
+const addFiltersButtons = () => {
+  imgSortFilters.classList.remove('img-filters--inactive');
+  imgSortFilters.classList.remove('visually-hidden');
+};
+
+buttonDiscussed.addEventListener('click', () => {
+  buttonDiscussed.classList.add('img-filters__button--active');
+  buttonDefault.classList.remove('img-filters__button--active');
+  buttonRandom.classList.remove('img-filters__button--active');
+});
+
+buttonDefault.addEventListener('click', () => {
+  buttonDefault.classList.add('img-filters__button--active');
+  buttonDiscussed.classList.remove('img-filters__button--active');
+  buttonRandom.classList.remove('img-filters__button--active');
+});
+
+buttonRandom.addEventListener('click', () => {
+  buttonRandom.classList.add('img-filters__button--active');
+  buttonDefault.classList.remove('img-filters__button--active');
+  buttonDiscussed.classList.remove('img-filters__button--active');
+});
+
+export {addPictures, addFiltersButtons};
